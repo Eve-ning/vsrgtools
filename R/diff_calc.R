254da10cf6940d0672936142e4b8c34ee11a92e6
Evening <johnchangqi@hotmail.com>
2019-06-12 23:43:34 +0800

    Give access to function instead of forcing quant

--- R/diff_calc.R
+++ R/diff_calc.R
@@ -3,46 +3,40 @@
 #' @param osu.stress Get this data.frame from stressSim
 #' @param osu.bcst Get this data.frame from diffBroadcast
 #' @param bin.size Size of the bin
-#' @param quant.threshold For Stress and BCst, at what
-#' quantile should the value be grabbed from
-#' @param alpha.threshold For the difficulty, what quantile
-#' should the value be grabbed from
-#'
+#' @param f.merge.key Specify the function used to merge
+#' key values together
+#' @param f.merge.bin Specify the function used to merge
+#' bins values together
 #' @export
 
 diffCalc <- function(osu.stress,
                      osu.bcst,
-                     bin.size = 5000,
-                     quant.threshold = 0.95,
-                     alpha.threshold = 0.95) {
-
+                     f.merge.key,
+                     f.merge.bin,
+                     bin.size = 5000) {
   require(dplyr)
   require(magrittr)
   require(reshape2)
 
   osu.stress %<>%
     group_by(keys, offsets) %>%
-    summarise(stress = quantile(stress, quant.threshold)) %>%
+    summarise(stress = f.merge.key(stress)) %>%
     mutate(bins = (offsets %/% bin.size) * bin.size) %>%
     group_by(bins) %>%
-    summarise(stress = mean(stress))
+    summarise(stress = f.merge.bin(stress))
 
   osu.bcst %<>%
     filter(keys.froms == keys.tos) %>%
     mutate(jack.inv = 1 / diffs) %>%
     group_by(keys.froms, offsets) %>%
-    summarise(jack.inv = quantile(jack.inv, quant.threshold)) %>%
+    summarise(jack.inv = f.merge.key(jack.inv)) %>%
     mutate(bins = (offsets %/% bin.size) * bin.size) %>%
     group_by(bins) %>%
-    summarise(jack.inv = mean(jack.inv))
+    summarise(jack.inv = f.merge.bin(jack.inv))
 
   osu.j <- merge(osu.stress, osu.bcst, by = 'bins')
 
-  # Attempt on diff calc
-  alpha <- quantile(osu.j$stress, alpha.threshold) *
-    quantile(osu.j$jack.inv, alpha.threshold)
-
-  return(list("graph" = osu.j, "alpha" = alpha))
+  return(osu.j)
 }
 
 
