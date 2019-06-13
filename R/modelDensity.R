063c6081e2991c1999846c9c494d360e04ed4e8a
Evening <johnchangqi@hotmail.com>
2019-06-13 17:30:42 +0800

    Add comments for clarity

--- R/modelDensity.R
+++ R/modelDensity.R
@@ -15,16 +15,26 @@ modelDensity <- function(chart, window = 1000) {
   require(reshape2)
 
   chart %<>%
+
+    # The cast and melt is to add offsets to types that dont
+    # have offsets
     dcast(offsets + keys ~ types,
           value.var = 'offsets',
           fun.aggregate = length) %>%
     melt(id.vars = 1:2,
          variable.name = "types",
          value.name = "value") %>%
+
+    # Separate by types to do moving average
     group_by(types) %>%
+
+    # This calculates the moving average, with a bool, but excludes
+    # extrapolated values by multiplying with the value
     mutate(no.objects = sapply(offsets, function(x){
                           sum((offsets <= x & offsets >= x - window) * value)
                         })) %>%
+
+    # Cast back to wide table
     dcast(offsets + keys ~ types,
           value.var = 'no.objects',
           fun.aggregate = first)
