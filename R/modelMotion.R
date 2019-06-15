0aa5285c8dca356943c599cd2c96a852e344c89e
Evening <johnchangqi@hotmail.com>
2019-06-16 01:23:36 +0800

    Create accessibility to summary functions

--- R/modelMotion.R
+++ R/modelMotion.R
@@ -27,14 +27,44 @@
 #' @param suppress.scale If suppress is True, this will
 #' expand the x range. In other words, smaller diffs will
 #' have a lower diffs.inv when this increases.
+#' @param is.summarised A Logical to indicate if the chart
+#' produced should be summarized by motions.
+#' @param summarised.char.fun The function to use to group
+#' different diffs.invs together. This function must
+#' contain (diffs.invs, froms, tos) as function argument and
+#' output a numeric for diffs.invs.
 #'
+#' Only one of the summarised.fun is needed to summarise.
+#'
+#' froms and tos are character types, the initials of each
+#' finger then the hand number indicates the character
+#'
+#' E.g. M1 is Middle Finger on Hand 1 (usually left hand).
+#'
+#' P1, R1, M1, I1, T1, T2, I2, M2, R2, P2 for all fingers.
+#'
+#' If it's NA, it'll default to a quantile(diffs.invs, 0.85)
+#' @param summarised.num.fun The function to use to group
+#' different diffs.invs together. This function must
+#' contain (diffs.invs, keys.froms, keys.tos) as function
+#' argument and output a numeric for diffs.invs.
+#'
+#' Only one of the summarised.fun is needed to summarise.
+#'
+#' keys.froms and keys.tos are numeric types, they indicate
+#' the column indexes
+#'
+#' If it's NA, it'll default to a quantile(diffs.invs, 0.85)
 #' @export
 
 model.motion <- function(chart.bcst, move.mapping = NA,
                          keyset.select = NA,
                          suppress = T,
                          suppress.threshold = 50,
-                         suppress.scale = 2.0){
+                         suppress.scale = 2.0,
+                         is.summarised = T,
+                         summarised.char.fun = NA,
+                         summarised.num.fun = NA){
 
   # Preemptive check on correct arguments
   if (is.na(move.mapping) & is.na(keyset.select)) {
@@ -62,6 +92,29 @@ model.motion <- function(chart.bcst, move.mapping = NA,
                           * suppress.scale)
                          - suppress.threshold)
                     ))
+
+  if(is.summarised) {
+
+    # Defaulted function if NA
+    if(!is.na(summarised.char.fun)) {
+      chart.motion %<>%
+        group_by(offsets) %>%
+        summarise(diffs.invs = summarised.char.fun(diffs.invs = diffs.invs,
+                                                   froms = froms,
+                                                   tos = tos))
+    } else if (!is.na(summarised.num.fun)) {
+      chart.motion %<>%
+        group_by(offsets) %>%
+        summarise(counts = summarised.num.fun(diffs.invs = diffs.invs,
+                                              keys.froms = keys.froms,
+                                              keys.tos = keys.tos))
+    } else {
+      chart.motion %<>%
+        group_by(offsets) %>%
+        summarise(diffs.invs = quantile(diffs.invs, 0.85))
+    }
+
+  }
 
   return(chart.motion)
 }
