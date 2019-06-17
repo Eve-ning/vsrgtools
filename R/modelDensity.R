891a9df8c94b28fdd8aa1d5b544bdf74263430d7
Evening <johnchangqi@hotmail.com>
2019-06-17 18:59:15 +0800

    Drop Summarization
    Closes #3

--- R/modelDensity.R
+++ R/modelDensity.R
@@ -7,22 +7,9 @@
 #' time frame to be processed.
 #' @param chart The chart generated from chartParse
 #' @param window The window to check for objects
-#' @param is.summarised A Logical to indicate if the chart
-#' produced should be summarized by types.
-#' @param summarised.fun The function to use to group
-#' different types of objects together. This function must
-#' contain (counts, types) as function arguments and output
-#' a numeric for counts.
-#'
-#' Note that types are character types, please use the
-#' names given through chartParse for conditionals
-#'
-#' If it's NA, it'll default to a normal sum of all objects
 #' @export
 
-model.density <- function(chart, window = 1000,
-                          is.summarised = T,
-                          summarised.fun = NA) {
+model.density <- function(chart, window = 1000) {
   require(dplyr)
 
   unq_offsets <- unique(chart$offsets)
@@ -40,23 +27,5 @@ model.density <- function(chart, window = 1000,
   }
 
   chart <- bind_rows(chart)
-
-  if(is.summarised) {
-    require(magrittr)
-    require(dplyr)
-
-    # Defaulted function if NA
-    if(is.na(summarised.fun)) {
-      summarised.fun <- function(counts, types) {
-        return(sum(counts))
-      }
-    }
-
-    chart %<>%
-      group_by(offsets) %>%
-      summarise(counts = summarised.fun(counts = counts,
-                                        types = types))
-  }
-
   return(chart)
 }
--- R/modelJackInv.R
+++ R/modelJackInv.R
@@ -3,20 +3,9 @@
 #' @description This uses diffBroadcast and just inverses
 #' the difference between equal keys.froms and keys.tos
 #' @param chart.bcst The chart generated from diffBroadcast
-#' @param is.summarised A Logical to indicate if the chart
-#' produced should be summarized by keys.
-#' @param summarised.fun The function to use to group
-#' different jack.invs together. This function must
-#' contain (jack.invs) as function argument and output
-#' a numeric for jack.invs.
-#'
-#' If it's NA, it'll default to a quantile(jack.invs, 0.85)
-#'
 #' @export
 
-model.jackInv <- function(chart.bcst,
-                          is.summarised = T,
-                          summarised.fun = NA){
+model.jackInv <- function(chart.bcst){
   require(magrittr)
   require(dplyr)
 
@@ -25,21 +14,6 @@ model.jackInv <- function(chart.bcst,
     rename(keys = keys.froms) %>%
     mutate(jack.invs = 1/diffs) %>%
     select(-c(types, diffs))
-
-  # We summarize the values here
-  if (is.summarised){
-
-    # Defaulted function if NA
-    if(is.na(summarised.fun)) {
-      summarised.fun <- function(jack.invs) {
-        return(quantile(jack.invs, 0.85))
-      }
-    }
-
-    chart.bcst %<>%
-      group_by(offsets) %>%
-      summarise(jack.invs = summarised.fun(jack.invs = jack.invs))
-  }
 
   return(chart.bcst)
 }
