766a74e4f417fd88917f3a2962f90b680def6c8c
Evening <johnchangqi@hotmail.com>
2019-06-17 22:39:55 +0800

    Fix Bad initialization error

--- src/model_density.cpp
+++ src/model_density.cpp
@@ -19,23 +19,23 @@ NumericVector cppModelDensity(NumericVector unq_offsets,
   }
 
   // Preallocate counts as output
-  NumericVector counts_out(unq_offsets.length(), 0);
+  NumericVector counts_out(unq_offsets.length(), 0.0);
 
   // We need iterators for both vectors
-  double window_start = 0;
-  double window_end = 0;
+  double window_start = 0.0;
+  double window_end = 0.0;
 
   unsigned int counts = 0;
 
-  for (unsigned int i = 0; i < unq_offsets.length(); i++){
+  for (int i = 0; i < unq_offsets.length(); i++){
     window_start = unq_offsets[i];
     window_end = window_start + window;
     counts = 0;
 
-    for (unsigned int j = 0; j < obj_offsets.length(); j++){
-      if (obj_offsets[j] >= window_start and
+    for (int j = 0; j < obj_offsets.length(); j++){
+      if (obj_offsets[j] >= window_start &&
           obj_offsets[j] < window_end) {
-        counts++;
+        counts += 1;
       }
     }
 
