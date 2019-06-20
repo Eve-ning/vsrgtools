fec19c631e0cd782ffc123c1c465a452a245b50e
Evening <johnchangqi@hotmail.com>
2019-06-20 18:52:11 +0800

    Implement summarisation

--- R/modelDensity.R
+++ R/modelDensity.R
@@ -11,8 +11,16 @@
 #' should be parsed separately
 #' @param mini.ln.threshold A numeric indicating the
 #' threshold of LNs to be considered mini
+#' @param mini.ln.tail.drop Logical indicating if the
+#' miniLN Tail should be dropped.
+#' @param mapping A data.frame to be merged with the output
+#' to generate weights.
 #'
-#' @importFrom dplyr bind_rows mutate distinct
+#' It might hold the columns types and weights
+#'
+#' If NA, .dflt.model.density.mapping will be used
+#'
+#' @importFrom dplyr bind_rows mutate distinct filter if_else
 #' @importFrom magrittr %<>%
 #' @importFrom rlang .data
 #'
@@ -20,15 +28,20 @@
 
 model.density <- function(chart.ext, window = 1000,
                           mini.ln.parse = T,
-                          mini.ln.threshold = 150) {
+                          mini.ln.threshold = 150,
+                          mini.ln.tail.drop = T,
+                          mapping = NA) {
 
   if (mini.ln.parse){
     chart.ext %<>%
       dplyr::mutate(
         types = ifelse((.data$types == 'lnoteh') & (.data$len <= mini.ln.threshold),
-                       'm.lnoteh', .data$types),
+                       'm.lnote', .data$types),
         types = ifelse((.data$types == 'lnotel') & (.data$len <= mini.ln.threshold),
                        'm.lnotel', .data$types))
+    if (mini.ln.tail.drop){
+      chart.ext %<>% dplyr::filter(.data$types != 'm.lnotel')
+    }
   }
 
   chart.ext %<>%
@@ -50,5 +63,15 @@ model.density <- function(chart.ext, window = 1000,
   }
 
   chart.ext <- dplyr::bind_rows(chart.ext)
+
+  chart.ext %<>%
+    merge(
+      dplyr::if_else(is.na(mapping), .dflt.model.density.mapping(), mapping),
+      by = 'types'
+    ) %>%
+    dplyr::mutate(values = .data$counts * .data$weights) %>%
+    dplyr::group_by(.data$offsets) %>%
+    dplyr::summarise(values = sum(.data$values))
+
   return(chart.ext)
 }
