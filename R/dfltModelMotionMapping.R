2756bd610b805e3856600e531279798f6bba3535
Evening <johnchangqi@hotmail.com>
2019-06-20 23:32:18 +0800

    Adjust weights, attempt on jacks also

--- R/dfltModelMotionMapping.R
+++ R/dfltModelMotionMapping.R
@@ -3,8 +3,8 @@
 .dflt.mtn.mapping <- function() {
   return(
     data.frame(
-      directions = c('across', 'in', 'out'),
-      weights = c(1.1, 1, 1.5)
+      directions = c('across', 'in', 'out', 'jack'),
+      weights = c(0.7, 1, 1.3, 3.0)
     )
   )
 }
