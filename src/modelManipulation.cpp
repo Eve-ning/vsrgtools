ebf949a6c5348e8d2b624bc1c9a5b5979192873e
Evening <johnchangqi@hotmail.com>
2019-06-27 23:33:58 +0800

    Create Framework for cpp manip

--- src/modelManipulation.cpp
+++ src/modelManipulation.cpp
@@ -1,11 +1,34 @@
 #include <Rcpp.h>
+#include "modelDensity.h"
 using namespace Rcpp;
 
 
-// [[-------------------Rcpp::export]]
+//' Helps model.manipulation in speeding up operations
+//'
+// [[Rcpp::export(name="cppModelManipulation")]]
+List cppModelManipulation(NumericVector offsets,
+                          List obj_offsets){
 
+  List out = List::create(_["offsets"] = offsets);
 
-NumericVector timesTwo(NumericVector x) {
-  return x * 2;
+  for(int i=0; i<obj_offsets.length(); i++) {
+    DataFrame index = as<DataFrame>(obj_offsets[i]);
+    out.push_back(cppModelDensity(offsets, index["offsets"], 1000, false));
+  }
+
+  return out;
+  //
+  // DataFrame groups(df.attr("groups"));
+  // List rows = groups[groups.size()-1];
+  // std::cout << groups.size()-1;
+  // int n = groups.nrow();
+  // List out;
+  //
+  //
+  //
+  // for(int i=0; i<n; i++) {
+  //   NumericVector index = rows[i];
+  //   out.push_back(cppModelDensity(offsets, index, 1000, false));
+  // }
+  // return out;
 }
-
