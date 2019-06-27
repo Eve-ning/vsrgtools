c025b916a98bedea8996b3c77c88e3148ff22742
Evening <johnchangqi@hotmail.com>
2019-06-27 23:57:13 +0800

    Drop countforward

--- src/modelDensity.cpp
+++ src/modelDensity.cpp
@@ -1,45 +1,7 @@
 #include <Rcpp.h>
-#include "modelDensity.h"
+#include "countForward.h"
 #include <algorithm>
 using namespace Rcpp;
-
-NumericVector cppCountForward(NumericVector unq_offsets,
-                              NumericVector obj_offsets,
-                              double window,
-                              bool is_sorted) {
-
-  // Sort them if they are not sorted
-  if (!is_sorted){
-    unq_offsets = unq_offsets.sort();
-    obj_offsets = obj_offsets.sort();
-  }
-
-  // Preallocate counts as output
-  NumericVector counts_out(unq_offsets.length(), 0.0);
-
-  // We need iterators for both vectors
-  double window_start = 0.0;
-  double window_end = 0.0;
-
-  unsigned int counts = 0;
-
-  for (int i = 0; i < unq_offsets.length(); i++){
-    window_start = unq_offsets[i];
-    window_end = window_start + window;
-    counts = 0;
-
-    for (int j = 0; j < obj_offsets.length(); j++){
-      if (obj_offsets[j] >= window_start &&
-          obj_offsets[j] < window_end) {
-        counts += 1;
-      }
-    }
-
-    counts_out[i] = counts;
-  }
-
-  return counts_out;
-}
 
 //' Counts the Density at each point of unq_offsets wrt obj_offsets
 //'
@@ -60,7 +22,7 @@ NumericVector cppCountForward(NumericVector unq_offsets,
 List cppModelDensity(NumericVector unq_offsets,
                      List obj_offsets,
                      double window,
-                     bool is_sorted) {
+                     bool is_sorted = false) {
 
   List out = List::create(_["offsets"] = unq_offsets);
 
