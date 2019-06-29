ed1820a2bca0a89fe10f19d57d9e2cfcbba10683
Evening <johnchangqi@hotmail.com>
2019-06-29 19:53:51 +0800

    Fix reinitialization and export issue

--- src/countForward.h
+++ src/countForward.h
@@ -1,7 +1,7 @@
+#pragma once
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// [[Rcpp::export]]
 NumericVector cppCountForward(NumericVector unq_offsets,
                               NumericVector obj_offsets,
                               double window,
