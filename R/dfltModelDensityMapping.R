f3b9811ee442106d3bd9fdb11b92dd46ca9c5ced
Evening <johnchangqi@hotmail.com>
2019-07-04 22:57:17 +0800

    May be deprecated after smoothing

--- R/dfltModelDensityMapping.R
+++ R/dfltModelDensityMapping.R
@@ -1,10 +1,11 @@
 #' Default Model Mapping for model.density
+#' May be deprecated after smoothing
 #' @export
 .dflt.dns.mapping <- function() {
   return(
     data.frame(
-      types = c('lnoteh', 'lnotel', 'm.lnote', 'm.lnotel', 'note'),
-      weights = rep(1,5),
+      types = c('lnoteh', 'lnotel', 'note'),
+      weights = rep(1,3),
       stringsAsFactors = F
     )
   )
