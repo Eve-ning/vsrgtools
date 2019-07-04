53d90228a9ac6af09ee03c60003ced9bbccfc0f6
Evening <johnchangqi@hotmail.com>
2019-07-04 23:44:19 +0800

    Add weight counting capability

--- src/countForward.cpp
+++ src/countForward.cpp
@@ -2,11 +2,12 @@
 #include "countForward.h"
 using namespace Rcpp;
 
-// [[Rcpp::export]]
+// [[Rcpp::export(name=".cppCountForward")]]
 NumericVector cppCountForward(NumericVector unq_offsets,
                               NumericVector obj_offsets,
                               double window,
-                              bool is_sorted) {
+                              bool is_sorted,
+                              Nullable<NumericVector> obj_weights) {
 
   // Sort them if they are not sorted
   if (!is_sorted){
@@ -21,22 +22,40 @@ NumericVector cppCountForward(NumericVector unq_offsets,
   double window_start = 0.0;
   double window_end = 0.0;
 
-  unsigned int counts = 0;
+  double counts = 0.0;
 
-  for (int i = 0; i < unq_offsets.length(); i++){
-    window_start = unq_offsets[i];
-    window_end = window_start + window;
-    counts = 0;
+  // We do a top level check so that the check isn't done multiple times
+  if (obj_weights.isNotNull()){ // Case when obj_weights is specified
+    NumericVector obj_weights_(obj_weights);
+    for (int i = 0; i < unq_offsets.length(); i++){
+      window_start = unq_offsets[i];
+      window_end = window_start + window;
+      counts = 0;
 
-    for (int j = 0; j < obj_offsets.length(); j++){
-      if (obj_offsets[j] >= window_start &&
-          obj_offsets[j] < window_end) {
-        counts += 1;
+      for (int j = 0; j < obj_offsets.length(); j++){
+        if (obj_offsets[j] >= window_start &&
+            obj_offsets[j] < window_end) {
+          counts += obj_weights_[j]; // Here's the difference
+        }
       }
+
+      counts_out[i] = counts;
     }
+  } else { // Case when obj_weights is not specified
+    for (int i = 0; i < unq_offsets.length(); i++){
+      window_start = unq_offsets[i];
+      window_end = window_start + window;
+      counts = 0;
 
-    counts_out[i] = counts;
+      for (int j = 0; j < obj_offsets.length(); j++){
+        if (obj_offsets[j] >= window_start &&
+            obj_offsets[j] < window_end) {
+          counts += 1; // Here's the difference
+        }
+      }
+
+      counts_out[i] = counts;
+    }
   }
-
   return counts_out;
 }
